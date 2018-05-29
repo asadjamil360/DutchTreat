7037033180e1ca4373f645edf4ba33abac9ec419
Odd A Hartvigsen <padda76@users.noreply.github.com>
2018-05-30 00:56:39 +0200

    Added _Layout-page

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -9,5 +9,16 @@ namespace DutchTreat.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
     }
 }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -16,6 +16,11 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
 
             app.UseMvc(cfg =>
