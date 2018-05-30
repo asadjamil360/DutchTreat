33c3b6f0b51d072cdca99d89aa6e63003276ebf1
Odd A Hartvigsen <padda76@users.noreply.github.com>
2018-05-30 12:00:14 +0200

    Added Razor errorpage and Mockmailservice

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,20 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
+            ViewBag.Title = "Home Page";
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
+
             return View();
         }
 
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("odd@mail.com", model.Subject, $"Mail from {model.Name}({model.Email}). Subject:{model.Subject} - Message{model.Message}");
+                ViewBag.UserMessage = "Message sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        [HttpGet("about")]
         public IActionResult About()
         {
             ViewBag.Title = "About Us";
